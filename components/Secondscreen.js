f2f22978b994a5dcfe0e939ad69d7fda0d075537
Derrick Kyei <D-Rock@Derricks-MacBook-Pro.local>
2019-11-11 15:20:38 -0500

    changed output

--- components/Secondscreen.js
+++ components/Secondscreen.js
@@ -10,6 +10,7 @@ import React, { Component } from "react";
 import { TextInput, StyleSheet, View, Dimensions, TouchableOpacity, ScrollView} from "react-native";
 import { Table, TableWrapper, Row, Rows, Col, Cols, Cell } from "react-native-table-component";
 import { Container, Header, Content, Form, Item, Input, ListItem, Title, CheckBox, Body, Icon, Text, Picker, Button, Footer, FooterTab } from "native-base";
+import { bold } from "ansi-colors";
 
 export default class SecondScreen extends Component {
   static navigationOptions = {
@@ -22,21 +23,23 @@ export default class SecondScreen extends Component {
     super(props);
     const Nutrients2 = 
       (
-      <View style={{ flex: 1, flexDirection: "row", justifyContent: "space-around", alignItems: "center" }}>
-        <Text style={{ color: 'blue' }}>Surplus</Text>
-        <Text style={{ color: 'red' }}>Deficit</Text>
-        <Text style={{ color: 'green' }}> Balance </Text>
+      <View style={{ flex: 1, flexDirection: "column", justifyContent: "space-between", alignItems: "center" }}>
+        <Text style={{ color: '#28004f', fontWeight: "bold" }}>Nutrients</Text>
+        <Text style={{ color: 'blue', fontWeight: "bold" }}>surplus</Text>
+        <Text style={{ color: 'black', fontWeight: "bold" }}>or</Text>
+        <Text style={{ color: 'red', fontWeight: "bold" }}> deficit </Text>
       </View>
       )
     const Nutrients1 = 
       (
-      <View style={{ flex: 1, flexDirection: "row", justifyContent: "center", alignItems: "center" }}>
-        <Text> Nutrients Supplied</Text>
+      <View style={{ flex: 1, flexDirection: "column", justifyContent: "center", alignItems: "center" }}>
+        <Text style={{fontWeight: "bold"}}> Nutrients</Text>
+        <Text style={{fontWeight: "bold"}}> Supplied</Text>
       </View>
       )
     this.state = {
-      nutrientsSuppliedLabel: [Nutrients1, Nutrients2],
-      recommendations: ["Recommendation", "N", "P", "K", "N", "P", "K","Score"],
+      nutrientsSuppliedLabel: ["Recommendation", Nutrients1, Nutrients2, "Score"],
+      recommendations: ["↓", "N", "P", "K", "N", "P", "K","↓"],
       currentNValue: this.props.navigation.state.params.currentNValue,
       solutions: this.props.navigation.state.params.solutions,
       output: this.props.navigation.state.params.output,
@@ -107,10 +110,12 @@ export default class SecondScreen extends Component {
 
         <View>            
             <Table borderStyle = {{borderWidth: 1}}>
-            <TableWrapper>
-              <Row data= {["Fertilizer Recommendations Based on Available Grades"]} style={{height: 40, backgroundColor: "#4f92ff"}} textStyle={[styles.text, {color: "white"}]}></Row>
-              <Row data={state.nutrientsSuppliedLabel}  flexArr={[1.25,1]} style={styles.head} textStyle={styles.text} />
-              <Row data={state.recommendations} flexArr={[2,1,1,1,1,1,1,1]} style={styles.head} textStyle={styles.text} />
+        
+          
+            <TableWrapper >
+              <Row data= {["Fertilizer Recommendations Based on Available Grades"]} style={{height: 40, backgroundColor: "#4f92ff",}} textStyle={[styles.text, {color: "white", fontWeight: "bold"}]}></Row>
+              <Row data={state.nutrientsSuppliedLabel}  flexArr={[2,3,3,1]} style={[styles.head, {height: 70}]} textStyle={styles.text1} />
+              <Row data={state.recommendations} flexArr={[2.02,1,1,1,1,1,1,1]} style={styles.head} textStyle={styles.text1} />
               </TableWrapper>
               <TableWrapper>
                  <Rows data={state.output} flexArr={[2,1,1,1,1,1,1,1]} textStyle={styles.text} style={{borderBottomWidth: 2, borderTopWidth: 2, borderBottomColor: "black", borderTopColor: "grey"}} />
@@ -127,7 +132,10 @@ export default class SecondScreen extends Component {
 const styles = StyleSheet.create({
   container: { flex: 1, padding: 16, backgroundColor: "#fff" },
   head: { height: 40, backgroundColor: "#f1f8ff" },
-  text: { margin: 2, textAlign: "center" },
+  head2: { flex: 1, backgroundColor: '#c8e1ff' },
+  title: { flex: 2, backgroundColor: '#f6f8fa' },
+  text: { margin: 2, textAlign: "center",},
+  text1: { margin: 2, textAlign: "center", fontWeight: "bold" },
   horizontalView: { flexDirection: 'row' },
   verticalView: {flexDirection: "column"},
   centerView: { flex: 1, justifyContent: "center", alignItems: "center" },
